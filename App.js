99f3ebb9be53caa04aab369728d57177c9398937
Aiman Md Uslim <amduslim@purdue.edu>
2017-07-30 16:42:49 +0800

    Basic form done

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Spinner, Item, Input } from 'native-base';
+import { Font } from 'expo';
+
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Roboto_medium': require('./node_modules/native-base/Fonts/Roboto_medium.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
+    if (!this.state.fontLoaded) {
+      return ( 
+        <Container style={{alignItems: 'center', justifyContent: 'center'}}>
+          <Spinner color='red' />
+        </Container> 
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Container style={{paddingTop: 25}}>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Makan Mana?</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Item regular>
+            <Input placeholder='Postcode' />
+          </Item>
+          <Item regular>
+            <Input placeholder='Area Name' />
+          </Item>
+          <Button full>
+            <Text>Choose For Me!</Text>
+          </Button>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
@@ -14,8 +67,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  myrow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
