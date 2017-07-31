b00117fd743100feba8ba0241f2a04712ff9be71
Aiman Md Uslim <amduslim@pudue.edu>
2017-08-01 00:05:26 +0800

    List workingcd makan-mana/

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Spinner, Item, Input } from 'native-base';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Spinner, Item, Input, List, ListItem} from 'native-base';
 import { Font } from 'expo';
+import axios from 'axios';
 
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      fontLoaded: false
+      fontLoaded: false, 
+      textToShow: 'original',
+      places: []
     };
   }
 
@@ -18,6 +21,27 @@ export default class App extends React.Component {
     });
 
     this.setState({ fontLoaded: true });
+  }
+
+  updateText = () => {
+    if(this.state.textToShow == 'original'){
+      this.setState({textToShow: 'change'})  
+    } else {
+      this.setState({textToShow: 'original'})  
+    }
+    
+  }
+
+  getPlacesList = () => {
+    axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyBhv0g9r-mtRqAEywP8bF8HSXvO0P9Wfro')
+      .then(response => {
+        console.log(response.data.results)
+        this.setState({ places: response.data.results })
+      });
+  }
+
+  renderPlaces () {
+    return this.state.places.map(place => <Text>{place.name}</Text>)
   }
 
   render() {
@@ -48,10 +72,19 @@ export default class App extends React.Component {
           <Item regular>
             <Input placeholder='Area Name' />
           </Item>
-          <Button full>
+          <Button full onPress={this.getPlacesList}>
             <Text>Choose For Me!</Text>
           </Button>
+           <List dataArray={this.state.places}
+            renderRow={(place) =>
+              <ListItem>
+                <Text>{place.name}</Text>
+              </ListItem>
+            }>
+            </List>
         </Content>
+
+
         <Footer>
           <FooterTab>
             <Button full>
