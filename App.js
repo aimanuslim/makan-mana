12a83073d63081e6dfb608d27852f405c0b6ffc4
Aiman Md Uslim <amduslim@pudue.edu>
2017-08-23 22:50:38 +0800

    implemented absolute distance calculation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, TextInput, ScrollView, Keyboard } from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Spinner, Item, Input, List, ListItem} from 'native-base';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Spinner, Item, Input, List, ListItem, Card} from 'native-base';
 import Display from './components/Display';
 import { Font } from 'expo';
 import axios from 'axios';
@@ -106,17 +106,22 @@ export default class App extends React.Component {
     }
   }
 
+  
+
+
 
   renderCard() {
     if (this.state.foundPlaces) {
       console.log("Random number: " + this.state.randomNumber)
       let detail = this.state.places[this.state.randomNumber];
+      let distance = findDistance(detail.geometry.location, {lat : this.state.myLatitude, lng : this.state.myLongitude});
       console.log("Detail: "  + detail + "Name: " + detail.name)
       console.log("Places " + this.state.places + "END END")
       return (
-        <View>
-          <Text  style={{color: 'blue', fontSize: 40}}>{detail.name}</Text>
-        </View>
+        <Card>
+          <Text  style={{color: 'blue', fontSize: 25, position: 'absolute', left: 0, top: 0}}>{detail.name}</Text>
+          <Text  style={{color: 'green', fontSize: 15, position: 'absolute', right: 0, bottom: 0}}>Absolute distance: {(distance * 1000).toFixed(2)} m</Text>
+        </Card>
       );
     } else if (this.state.resultsUnfound) {
       return (<Text>Results cannot be found. Please try again</Text>);
@@ -335,3 +340,30 @@ const styles = StyleSheet.create({
     margin: 15
   }
 });
+
+
+function findDistance(location1, location2){
+      var lat1  = location1.lat;
+      var lat2  = location2.lat;
+      var lon1  = location1.lng;
+      var lon2  = location2.lng;
+      console.log("lat1: " + lat1 + "lat2: " + lat2 + "lon1: " + lon1 + "lon2: " + lon2);
+
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2-lat1);  // deg2rad below
+      var dLon = deg2rad(lon2-lon1); 
+      var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+      var d = R * c; // Distance in km
+      return d;
+
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
